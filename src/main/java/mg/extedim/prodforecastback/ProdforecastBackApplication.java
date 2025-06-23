24011de1635364511ee8c2c6b3edb8f71cfe6a1f
Princy <rafamatanantsoa10@gmail.com>
2025-06-23 15:22:11 +0300

    Initial commit

---
+++ src/main/java/mg/extedim/prodforecastback/ProdforecastBackApplication.java
@@ -0,0 +1,13 @@
+package mg.extedim.prodforecastback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdforecastBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdforecastBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/extedim/prodforecastback/ProdforecastBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.extedim.prodforecastback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdforecastBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
